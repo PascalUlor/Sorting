8664afb02659278aa8001b7c54c6b97158975fda
Lambda <lambda@Abrahams-MacBook-Pro.local>
2019-10-07 19:04:32 +0100

    solve buble sort algorithm

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -18,6 +18,14 @@ def selection_sort(arr):
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
+    for i in range(0, len(arr) - 1):
+        cur_index = i
+        smallest_index = cur_index
+        for j in range(cur_index, len(arr)):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
+
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
 
